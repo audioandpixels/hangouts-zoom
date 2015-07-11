3e34efe4c1464a64896871a634df9cf007275a76
jasoncox <jason@audioandpixels.com>
2015-07-11 11:58:21 +1000

    Trial zoom settings

--- zoom.js
+++ zoom.js
@@ -2,34 +2,14 @@ function simple() {
   var metaEffect = gapi.hangout.av.effects.createMetaEffect();
   var effectChain = [];
 
-  var bcsProps = {
-    'target_brightness': 150, /* 0 .. 255 */
-    'brightness': 0.5, /* -1 .. 1 */
-    'constrast': 0.5, /* -1 .. 1 */
-    'saturation': 1.0 /* -1 .. 1 */
+  var zoomProps = {
+    'height': 300,
+    'preserve_aspect_ratio': true
   };
 
-  var simpleEffect = metaEffect.createSubEffect('auto_bcs', bcsProps);
-  effectChain.push(simpleEffect);
-
-  var vignetteProps = {
-    color: { 'r': 0, 'g': 0, 'b': 0},
-    focus: { 'x': 0.5, 'y': 0.5 },
-    size: 0.7
-  };
-
-  var extraEffect = metaEffect.createSubEffect('vignetting', vignetteProps);
-  effectChain.push(extraEffect);
+  var zoomEffect = metaEffect.createSubEffect('resize', bcsProps);
+  effectChain.push(zoomEffect);
 
   metaEffect.initEffects(effectChain);
   metaEffect.pipelineEffects(effectChain);
 }
-
-function init() {
-  gapi.hangout.onApiReady.add(function(eventObj) {
-    console.log("Hangouts Zoom Ready");
-    document.querySelector('#fullUI').style.visibility = 'visible';
-  });
-}
-
-gadgets.util.registerOnLoadHandler(init);
