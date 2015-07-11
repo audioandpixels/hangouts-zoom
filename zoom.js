f114476ab92a00098c2f050844dc5e2b73eaf2d1
jasoncox <jason@audioandpixels.com>
2015-07-11 13:23:00 +1000

    Try to get effect desc

--- zoom.js
+++ zoom.js
@@ -1,9 +1,26 @@
 function simple() {
+  var detectedEffectDescription = {};
+
+  var onNotify = function(notify) {
+    if (notify != null && notify.effect_descriptions != null) {
+      detectedEffectDescriptions = notify.effect_descriptions;
+
+      console.log(defectedEffectDescriptions);
+
+      // Effect descriptions have been received, remove this callback.
+      g_metaEffect.onNotify.remove(onNotify);
+    }
+  };
+
+
   var ratio = document.getElementById("ratio").value;
   var xpos = document.getElementById("xpos").value;
   var ypos = document.getElementById("ypos").value;
   var metaEffect = gapi.hangout.av.effects.createMetaEffect();
   var effectChain = [];
+
+  metaEffect.onNotify.add(onNotify);
+  metaEffect.getEffectDescriptions();
 
   var cropProps = {
     topLeft: {
