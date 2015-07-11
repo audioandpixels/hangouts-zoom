c8e9b0a40bcb8fbdaed7c07a245ef05b3182d0a8
jasoncox <jason@audioandpixels.com>
2015-07-11 13:58:43 +1000

    Functional

--- zoom.js
+++ zoom.js
@@ -1,17 +1,14 @@
 function simple() {
-  var ratio = document.getElementById("ratio").value;
-  var xpos = document.getElementById("xpos").value;
-  var ypos = document.getElementById("ypos").value;
   var metaEffect = gapi.hangout.av.effects.createMetaEffect();
   var effectChain = [];
 
   var cropProps = {
     top_left: {
-      x: xpos,
-      y: ypos
+      x: 0.005,
+      y: 0.005
     },
-    width: ratio,
-    height: ratio
+    width: 0.99,
+    height: 0.99
   };
 
   var zoomEffect = metaEffect.createSubEffect('crop', cropProps);
